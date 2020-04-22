1444c1fc36aa01bb33aa6cf6a6d37706c67497bb
Sergey Vinogorov <vinogorich@gmail.comgit config --global user.email vinogorich@gmail.comgit config --global user.email vinogorich@gmail.com>
2020-04-22 23:28:46 +0300

    add simple notification components

---
+++ src/components/NotificationContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="notification-container">
+    <Notification
+      v-for="notification in notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+  </div>
+</template>
+
+<script>
+import Notification from "./Notification";
+import { mapState } from "vuex";
+export default {
+  components: {
+    Notification,
+  },
+  computed: mapState("notification", ["notifications"]),
+};
+</script>
+
+<style lang="scss" scoped>
+.notification-container {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding-right: 40px;
+}
+</style>
